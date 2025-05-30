8c781f2210f5e3792ea3d84ef644641732dd64f1
jairorocha <jairabvl@hotmail.com>
2025-05-30 16:27:47 -0300

    att

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (_, res) => res.sendFile(path.join(__dirname, 'views/index.html')));
-app.get('/sobre', (_, res) => res.sendFile(path.join(__dirname, 'views/sobre.html')));
+app.get('/', (_, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/sobre', (_, res) => res.sendFile(path.join(__dirname, 'sobre.html')));
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
 app.listen(port, () => {
